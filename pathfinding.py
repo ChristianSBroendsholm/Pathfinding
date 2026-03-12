ddcaa1582af5058327afc6ef5e62964517645505
Christian Sønderskov Brøndsholm, 2.n <christian.broendsholm@gmail.com>
2026-03-12 10:46:56 +0100

    Algoritme klasse med metoder, der laver en sti fra Start til Goal objekter. Mangler: Algoritme skal vælge én konkret sti, da den lige nu på hvert trin vælger hver node med lavest F cost, hvilket kan føre til unødvendige trin.

--- pathfinding.py
+++ pathfinding.py
@@ -10,43 +10,33 @@ width = screen.get_width()
 height = screen.get_height()
 
     
-spacing = 5
+#spacing = 5
 nodes = []
 
 class Grid:
-    def __init__(self, num_cells):
-        self.num_cells = num_cells
-        self.c_w = width / (self.num_cells + spacing) ** 0.5 - spacing + spacing / 2 / self.num_cells ** 0.5
-        self.c_h = height / (self.num_cells + spacing) ** 0.5 - spacing + spacing / 2 / self.num_cells ** 0.5
-        self.font = pg.font.Font(None, int(self.c_w / 1.5))
+    def __init__(self, num_nodes):
+        self.num_nodes = num_nodes
+        self.n_w = (width / self.num_nodes ** 0.5) * 0.8
+        self.n_h = (height / self.num_nodes ** 0.5) * 0.8
+        self.spacing = (width / self.num_nodes ** 0.5) * 0.2 + (self.n_w / self.num_nodes ** 0.5)
+        
+        #self.n_w = width / (self.num_nodes + spacing) ** 0.5 - spacing + spacing / 2 / self.num_nodes ** 0.5
+        #self.n_h = height / (self.num_nodes + spacing) ** 0.5 - spacing + spacing / 2 / self.num_nodes ** 0.5
+        self.font = pg.font.Font(None, int(self.n_w / 1.5))
 
         self.make_grid()
         
     def make_grid(self):
-        for i in range(self.num_cells):
-            c_x = spacing + (self.c_w + spacing) * (i % int(self.num_cells ** 0.5))
-            c_y = spacing + (self.c_h + spacing) * int(i / int(self.num_cells ** 0.5))
-            #print(0.1 - abs((len(cells) - self.num_cells / 2) / (self.num_cells * 1)))
-            if random.random() > 0.3 - abs((len(nodes) - self.num_cells / 2) / (self.num_cells * 1)): 
-                nodes.append(Node(c_x, c_y, self.c_w, self.c_h))
+        for i in range(self.num_nodes):
+            n_x = self.spacing + (self.n_w + self.spacing) * (i % int(self.num_nodes ** 0.5))
+            n_y = self.spacing + (self.n_h + self.spacing) * int(i / int(self.num_nodes ** 0.5))
+
+            if random.random() > 0.3 - abs((len(nodes) - self.num_nodes / 2) / (self.num_nodes * 1)): 
+                nodes.append(Node(n_x, n_y, self.n_w, self.n_h))
             else:
-                nodes.append(Obstacle(c_x, c_y, self.c_w, self.c_h))
+                nodes.append(Obstacle(n_x, n_y, self.n_w, self.n_h))
     
-
-    # Skal flyttes til Algorithm
-    def F_cost(self, cell, start, goal):
-        f = int(self.G_cost(cell, start) + self.H_cost(cell, goal))
-        #print(f)
-        cell.text = self.font.render(f"{f}", True, pg.Color("black"))
-
-    def G_cost(self, cell, start):
-        return ((start.x - cell.x) ** 2 + (start.y - cell.y) ** 2) ** 0.5
-
-    def H_cost(self, cell, goal):
-        return ((goal.x - cell.x) ** 2 + (goal.y - cell.y) ** 2) ** 0.5
         
-
-
 class Node:
     def __init__(self, x, y, w, h):
         self.x = x
@@ -91,61 +81,101 @@ class Obstacle(Node):
 class Goal(Node):
     def __init__(self, x, y, w, h):
         super().__init__(x, y, w, h)
+        self.text = grid.font.render("B", True, pg.Color("black"))
         self.color = pg.Color("red")
+
+    def draw(self):
+        pg.draw.rect(screen, self.color, self.rect)
+
+        if self.text:
+            screen.blit(self.text, (self.x + self.w / 3, self.y + self.h / 4))
 
 class Start(Node):
     def __init__(self, x, y, w, h):
         super().__init__(x, y, w, h)
+        self.text = grid.font.render("A", True, pg.Color("black"))
         self.color = pg.Color("green")
+        
+    def draw(self):
+        pg.draw.rect(screen, self.color, self.rect)
 
-    def next_cell(self):
-        goal_cell = None
-        self.index = nodes.index(self)
-        self.nb_i = (int(self.index + 1),
-                        int(self.index - 1),
-                        int(self.index + grid.num_cells ** 0.5),
-                        int(self.index - grid.num_cells ** 0.5),
-                        int(self.index + grid.num_cells ** 0.5 + 1),
-                        int(self.index + grid.num_cells ** 0.5 - 1),
-                        int(self.index - grid.num_cells ** 0.5 - 1),
-                        int(self.index - grid.num_cells ** 0.5 + 1))
+        if self.text:
+            screen.blit(self.text, (self.x + self.w / 3, self.y + self.h / 4))
 
-        goal_cell = [n for n in nodes if isinstance(n, Goal)][0]
-        #print(goal_cell)
-        if goal_cell:
-            for n in self.nb_i:
-                if nodes[n]:
-                    nb = nodes[n]
-                    if type(nb) == Node: 
-                        #print(c, self, goal_cell)
-                        grid.F_cost(nb, self, goal_cell)
-
-                        new_nb = Closed(nb.x, nb.y, nb.w, nb.h)
-                        new_nb.text = nb.text
-                        nodes[n] = new_nb
-        """
-        for c in cells:
-            if type(c) == Cell:
-                if goal_cell: 
-                    #print(c, self, goal_cell)
-                    grid.F_cost(c, self, goal_cell)
-        """
-
-class Closed(Start):
-    def __init__(self, x, y, w, h):
+class Closed(Node):
+    def __init__(self, x, y, w, h, F_cost):
         super().__init__(x, y, w, h)
+        self.F_cost = F_cost
+        self.text = grid.font.render(f"{self.F_cost}", True, pg.Color("black"))
         self.color = pg.Color("blue")
 
-grid = Grid(20 ** 2)
+class Chosen(Closed):
+    def __init__(self, x, y, w, h, F_cost):
+        super().__init__(x, y, w, h, F_cost)
+        self.color = pg.Color("green")
 
 class Algorithm:
-    pass
+    def __init__(self):
+        self.closed_list = []
+        self.done = False
+    
+    def F_cost(self, node, start, goal):
+        f = int(self.G_cost(node, start) + self.H_cost(node, goal))
+        return f
+
+    def G_cost(self, node, start):
+        return ((start.x - node.x) ** 2 + (start.y - node.y) ** 2) ** 0.5
+
+    def H_cost(self, node, goal):
+        return ((goal.x - node.x) ** 2 + (goal.y - node.y) ** 2) ** 0.5
+    
+    def next_node(self, node):
+        if node and self.done == False:
+            goal = [n for n in nodes if type(n) == Goal][0]
+            start = [n for n in nodes if type(n) == Start][0]
+            node_index = nodes.index(node)
+            nb_i = (int(node_index + 1), # Højre
+                    int(node_index - 1), # Venstre
+                    int(node_index + grid.num_nodes ** 0.5), # Under
+                    int(node_index - grid.num_nodes ** 0.5), # Over
+                    int(node_index + grid.num_nodes ** 0.5 + 1), # Diagonale noder
+                    int(node_index + grid.num_nodes ** 0.5 - 1),
+                    int(node_index - grid.num_nodes ** 0.5 - 1),
+                    int(node_index - grid.num_nodes ** 0.5 + 1))
+
+            if goal and start:
+                for i in nb_i:
+                    if nodes[i]:
+                        nb = nodes[i]
+                        if type(nb) == Goal:
+                            self.done = True
+                        if type(nb) == Node: 
+                            cost = self.F_cost(nb, start, goal)
+                            
+                            new_nb = Closed(nb.x, nb.y, nb.w, nb.h, cost)
+                            self.closed_list.append(new_nb)
+                            
+                            nodes[i] = new_nb
+    def best_nb(self):
+        if self.closed_list and self.done == False:
+            best = min(self.closed_list, key=lambda c_node: c_node.F_cost)
+            self.closed_list.remove(best)
+            #nodes[nodes.index(best)] = Closed(best.x, best.y, best.w, best.h, best.F_cost)
+            best.color = pg.Color("green")
+            #self.closed_list = []
+            return best
+    
+    def solve(self):
+        while self.done == False:
+            algorithm.next_node(algorithm.best_nb())
+
+    
 
 class Player:
     pass
 
-
-
+grid = Grid(20 ** 2)
+algorithm = Algorithm()
 
 #grid.F_cost(cells[20], cells[80], cells[200])
 
@@ -154,26 +184,26 @@ running = True
 
 while running:
 
-    for n in nodes:
-        n.update()
-
     for event in pg.event.get():
-        #print(event.type)
         for n in nodes:
             n.mouse(event)
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
-                
+                closed_exists = False
                 for n in nodes:
-                    if type(n) == Goal:
-                        print("rgfejuneirfgn")
                     if type(n) == Start:
-                        n.next_cell()
+                        algorithm.next_node(n)
+                    if type(n) == Closed:
+                        closed_exists = True
+                if closed_exists:
+                    algorithm.solve()
+                    #algorithm.next_node(algorithm.best_nb())
 
 
         if event.type == pg.QUIT:
             running = False
-        
+    for n in nodes:
+        n.update()
 
     pg.display.flip()
     clock.tick(60)
